289b64c942d2d59c8efcd94be327e5a10cae4b09
MariinDM <diegomaarinmontalvo@gmail.com>
2025-08-21 10:55:28 -0600

    [ADD] implement home layout with navbar and dashboard components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', loadComponent: () => import('./features/home/layout/home-layout/home-layout').then(m => m.HomeLayout) },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
---
+++ src/app/features/home/layout/home-layout/home-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navbar } from "../../../../shared/components/navbar/navbar";
+
+@Component({
+  selector: 'app-home-layout',
+  imports: [Navbar],
+  templateUrl: './home-layout.html',
+  styleUrl: './home-layout.scss'
+})
+export class HomeLayout {
+
+}

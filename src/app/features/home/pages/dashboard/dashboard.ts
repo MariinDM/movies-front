feb2b3da8558ca961b91273bf1a6ce3f47dfec9f
MariinDM <diegomaarinmontalvo@gmail.com>
2025-08-21 14:13:47 -0600

    [ADD] implement HomeService for movie data management and create unit tests for the service

--- src/app/features/home/pages/dashboard/dashboard.ts
+++ src/app/features/home/pages/dashboard/dashboard.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Movie } from '../../interfaces/movie';
+import { HomeService } from '../../services/home-service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,24 +10,7 @@ import { Movie } from '../../interfaces/movie';
 })
 export class Dashboard {
 
-  listMovies: Movie[] = [
-    { title: 'Como entrenar a tu dragón', img: '/list-movies/como-entrenar-a-tu-dragon.webp' },
-    { title: 'Elio', img: '/list-movies/elio.webp' },
-    { title: 'Jurassic World: Renace', img: '/list-movies/jurassic-world-renace.webp' },
-    { title: 'La Guerra de los Mundos', img: '/list-movies/la-guerra-de-los-mundos.webp' },
-    { title: 'La Hora de la Desaparición', img: '/list-movies/la-hora-de-la-desaparicion.webp' },
-    { title: 'Lilo y Stitch', img: '/list-movies/lilo-y-stitch.webp' },
-    { title: 'Los 4 Fantásticos', img: '/list-movies/los-4-fantasticos.webp' },
-    { title: 'Misión Imposible: Sentencia Final', img: '/list-movies/mision-imposible-sentencia-final.webp' },
-    { title: 'Los Pitufos', img: '/list-movies/pitufos.webp' },
-    { title: 'Rescate Impactante', img: '/list-movies/rescate-impactante.webp' },
-    { title: 'Robin', img: '/list-movies/robin.webp' },
-    { title: 'Spider-Man: A Través del Spider-Verso', img: '/list-movies/spiderman-a-atraves-del-spiderverso.webp' },
-    { title: 'Spider-Man: La Amenaza de Electro', img: '/list-movies/spiderman-2-la-amenaza-de-electro.webp' },
-    { title: 'Spider-Man: De Regreso a Casa', img: '/list-movies/spiderman-de-regreso-a-casa.webp' },
-    { title: 'Spider-Man: Sin Camino a Casa', img: '/list-movies/spiderman-sin-camino-a-casa.webp' },
-    { title: 'Spider-Man', img: '/list-movies/spiderman.webp' },
-    { title: 'Superman', img: '/list-movies/superman.webp' }
-  ];
+  private service = inject(HomeService);
+  protected movies = this.service.moviesSignal;
 
 }
